cf2465f8cb4249ee03ec6923cfecf3b81829addf
loadKK <1780319991@qq.com>
2022-10-04 01:09:15 +0800

    分组

--- examples/example-capstone/capstone-hello.py
+++ examples/example-capstone/capstone-hello.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2022-09-30 19:21
 # @Author  : KKings
-# @File    : Capstone_demo.py
+# @File    : capstone-hello.py
 # @Software: PyCharm
 
 from capstone import *
+
 CODE = b"\xf1\x02\x03\x0e\x00\x00\xa0\xe3\x02\x30\xc1\xe7\x00\x00\x53\xe3"
 
 md = Cs(CS_ARCH_ARM, CS_MODE_ARM)
 for i in md.disasm(CODE, 0x1000):
-    print("%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+    print("%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
